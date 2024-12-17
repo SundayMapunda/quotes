78634a762f703226341ba149cce2fe8bd6074287
Sunday <sunnyjumapili@gmail.com>
2024-12-17 13:20:51 +0300

    Under maintenance

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,7 @@
 import { Route, createRoutesFromElements } from "react-router-dom";
 // import MainLayout from "./layouts/MainLayout.jsx";
 // import App from "./App.jsx";
-import Quiz from "./pages/Quiz.jsx";
+// import Quiz from "./pages/Quiz.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 
 const routes = createRoutesFromElements(
@@ -9,9 +9,9 @@ const routes = createRoutesFromElements(
     {/* <Route path="/quotes/" element={<MainLayout />}>
       <Route index element={<App />} />
     </Route> */}
-    <Route path="/quotes" element={<Quiz />} />
-    <Route path="*" element={<ErrorPage />} />
-  </>
+    {/* <Route path="/quotes" element={<Quiz />} /> */}
+    <Route path="/quotes" element={<ErrorPage />} />
+    </>
 );
 
 export default routes;
