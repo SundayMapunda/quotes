043b91be429e4bae4878a96d81ebb92378c9f56a
Sunday <sunnyjumapili@gmail.com>
2025-01-26 19:54:51 +0300

    Back To Basic

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,18 +3,18 @@ import { Route, createRoutesFromElements } from "react-router-dom";
 // import App from "./App.jsx";
 // import Quiz from "./pages/Quiz.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
-import Birthday from "./pages/Birthday.jsx";
-import Wallpaper from "./pages/Wallpaper.jsx";
+// import Birthday from "./pages/Birthday.jsx";
+// import Wallpaper from "./pages/Wallpaper.jsx";
 
 const routes = createRoutesFromElements(
   <>
     {/* <Route path="/quotes/" element={<MainLayout />}  errorElement={<ErrorPage />}>
       <Route index element={<App />} />
-    </Route> */}
-    {/* <Route path="/quotes" element={<Quiz />} /> */}
-    {/* <Route path="*" element={<ErrorPage />} /> */}
-    <Route path="/quotes" element={<Birthday />} />
-    <Route path="/quotes/board" element={<Wallpaper />} />
+    </Route>
+    <Route path="/quotes" element={<Quiz />} /> */}
+    <Route path="*" element={<ErrorPage />} />
+    {/* <Route path="/quotes" element={<Birthday />} />
+    <Route path="/quotes/board" element={<Wallpaper />} /> */}
   </>
 );
 
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import giraffe from './giraffe.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import giraffe from "./giraffe.jpg";
 
 const ErrorPage = () => {
   return (
     <>
-    <div className="container">
-      <div className="picture my-3">
-        <span><small>Giraffe</small></span>
-        <img src={giraffe} alt="picture" className='img-fluid' />
-        <div className="title text-center my-3 display-5">
-          I'm not the only traveler, <br />
-          who has not repaid his debt.
+      <div className="container py-3">
+        <span>
+          <small>Giraffe</small>
+        </span>
+        <div className="picture text-center">
+          <img src={giraffe} alt="picture" className="img-fluid" />
+          <div className="title my-3 display-5">
+            I'm not the only traveler, <br />
+            who has not repaid his debt.
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
